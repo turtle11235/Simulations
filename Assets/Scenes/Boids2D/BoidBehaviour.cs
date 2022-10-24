d8cd59d509652842d165ed2a798ed97f7f71c1e4
Emrys Shevek <mpoggemann96@gmail.com>
2022-10-23 20:22:31 -0700

    almost working

--- Assets/Scenes/Boids2D/BoidBehaviour.cs
+++ Assets/Scenes/Boids2D/BoidBehaviour.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using UnityEngine;
 
@@ -5,9 +6,13 @@ public class BoidBehaviour : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float speed = 10;
-    public float turnRadius = 0.1f;
+    public float turnSpeed = 0.1f;
     public float visionRadius = 10f;
+    public float separationDistance = 2f;
 
+    private Vector3 sv;
+    private Vector3 av;
+    private Vector3 cv;
     private Vector3 desiredVector;
     private Vector3 currentVector;
     // Start is called before the first frame update
@@ -19,19 +24,21 @@ public class BoidBehaviour : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        currentVector = transform.TransformDirection(Vector3.right);
+        desiredVector = new Vector3(currentVector.x, currentVector.y, currentVector.z);
         Transform[] n = getNeighbors();
-        Vector3 s = separationVector(n);
-        //Vector3 a = alignmentVector(n);
-        ////Vector3 c = cohesionVector(n);
-        desiredVector = new(0, 0, 0);
-        desiredVector += s;
-        //desiredVector += a;
-        //desiredVector += c;
+        Debug.Log(n.Length + " neighbors");
+        sv = separationVector(n);
+        av = alignmentVector(n);
+        cv = cohesionVector(n);
+        desiredVector += sv;
+        desiredVector += av;
+        desiredVector += cv;
         //Vector3 desiredVector = separationVector(n) + alignmentVector(n) + cohesionVector(n);
-        Vector3 currentVector = AngleToVector(transform.rotation.eulerAngles.z);
 
-        float desiredRotation = Vector3.SignedAngle(currentVector, desiredVector, new Vector3(1, 0, 0));
-        float rotation = desiredRotation > 0 ? Mathf.Min(turnRadius, desiredRotation) : Mathf.Max(-turnRadius, desiredRotation);
+        float desiredAngle = Vector3.SignedAngle(Vector3.right, desiredVector, Vector3.right);
+        float desiredRotation = desiredAngle - transform.rotation.eulerAngles.z;
+        float rotation = desiredRotation > 0 ? Mathf.Min(turnSpeed, desiredRotation) : Mathf.Max(-turnSpeed, desiredRotation);
         transform.Rotate(new Vector3(0, 0, rotation));
         float currentAngle = transform.rotation.eulerAngles.z;
         currentVector = AngleToVector(currentAngle);
@@ -41,10 +48,25 @@ public class BoidBehaviour : MonoBehaviour
         
     }
 
-    void OnDrawGizmos()
+    void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.white;
         Gizmos.DrawWireSphere(transform.position, visionRadius);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, sv * 5);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(transform.position, av);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(transform.position, cv);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(transform.position, desiredVector);
+
+        Gizmos.color = Color.white;
+        Gizmos.DrawRay(transform.position, transform.TransformDirection(Vector3.right) * visionRadius);
     }
 
     float VectorToAngle(Vector3 v)
@@ -59,24 +81,27 @@ public class BoidBehaviour : MonoBehaviour
 
     Transform[] getNeighbors()
     {
-        return Physics.OverlapSphere(transform.position, visionRadius).Select(x => x.transform).ToArray();
+        Transform[] allTransforms = Physics2D.OverlapCircleAll(transform.position, visionRadius).Select(x => x.transform).ToArray();
+        return allTransforms.Where(x => x.gameObject != transform.gameObject).ToArray();
     }
 
     Vector3 separationVector(Transform[] neighbors)
     {
-        Vector3[] positionVectors = neighbors.Select(x => transform.position - x.position).ToArray();
-        return positionVectors.Aggregate(new Vector3(0,0,0), (acc, x) => acc - x);
+        Transform[] tooClose = neighbors.Where(x => Mathf.Abs(Vector3.Distance(x.position, transform.position)) < separationDistance).ToArray();
+        Vector3[] positionVectors = tooClose.Select(x => transform.position - x.position).ToArray();
+        return positionVectors.Aggregate(new Vector3(0,0,0), (acc, x) => acc + (separationDistance / x.magnitude * x.normalized) );
     }
 
     Vector3 alignmentVector(Transform[] neighbors)
     {
-        Vector3[] rotationVectors = neighbors.Select(x => x.forward.normalized).ToArray();
-        return rotationVectors.Aggregate(new Vector3(0,0,0), (acc, x) => acc += x);
+        Vector3[] rotationVectors = neighbors.Select(x => x.TransformDirection(Vector3.right)).ToArray();
+        Vector3 v = new Vector3(currentVector.x, currentVector.y);
+        return rotationVectors.Aggregate(new Vector3(0,0,0), (acc, x) => acc += x).normalized;
     }
 
     Vector3 cohesionVector(Transform[] neighbors)
     {
         Vector3[] positionVectors = neighbors.Select(x => x.position - transform.position).ToArray();
-        return positionVectors.Aggregate(new Vector3(0, 0, 0), (acc, x) => acc + x) / Mathf.Max(neighbors.Length, 1) / 100;
+        return positionVectors.Aggregate(new Vector3(0,0,0), (acc, x) => acc + x).normalized;
     }
 }
